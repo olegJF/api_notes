ee8af17852fc6f0604ebc418235abb26c9480a52
oleg <jf2@ua.fm>
2019-05-01 16:50:48 +0300

    Добавил приложение accounts

--- post_in/post_in/settings.py
+++ post_in/post_in/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'post_in.wsgi.application'
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Database
