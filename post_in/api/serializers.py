e5ff86bcbc50402c12bbe7c9238299f64f0719b4
oleg <jf2@ua.fm>
2019-05-12 11:29:12 +0300

    Создал приложение api

---
+++ post_in/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import (IntegerField, CharField, Serializer, ModelSerializer)
+from notes.models import Note
+
+class NoteSerializer(ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
+        
+# class NoteSerializer(Serializer):
+#     id = IntegerField(read_only=True)
+#     title = CharField(required=True, max_length=250)
+#     text = CharField(required=False, allow_blank=True)
+
+#     def create(self, validated_data):
+#         return Note.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.text = validated_data.get('text', instance.text)
+#         instance.save()
+#         return instance
--- post_in/post_in/settings.py
+++ post_in/post_in/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'notes',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
