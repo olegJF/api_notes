4cb21369fdaf61cbb4fb7ace0f7a748bcc718252
oleg <jf2@ua.fm>
2019-05-19 13:55:54 +0300

    Добавил тонкий сериализатор

--- post_in/api/serializers.py
+++ post_in/api/serializers.py
@@ -1,10 +1,19 @@
-from rest_framework.serializers import (IntegerField, CharField, Serializer, ModelSerializer)
+from rest_framework.serializers import (IntegerField, CharField, Serializer, 
+ModelSerializer, HyperlinkedIdentityField)
 from notes.models import Note
 
 class NoteSerializer(ModelSerializer):
     class Meta:
         model = Note
         fields = '__all__'
+
+
+class ThinNoteSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(view_name='notes-detail')
+
+    class Meta:
+        model = Note
+        fields = ('id', 'title', 'url')
         
 # class NoteSerializer(Serializer):
 #     id = IntegerField(read_only=True)
