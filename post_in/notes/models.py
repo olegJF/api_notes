7f58db03d47da7200a8a15df80acc9a26b863980
oleg <jf2@ua.fm>
2019-05-26 11:04:57 +0300

    Добавил автора в модель Note

--- post_in/notes/models.py
+++ post_in/notes/models.py
@@ -1,7 +1,11 @@
-from django.db.models import (Model, CharField, TextField, DateTimeField)
+from django.db.models import (Model, CharField, TextField, 
+DateTimeField, ForeignKey, SET_NULL)
+from django.conf import settings
 
+User = settings.AUTH_USER_MODEL
 
 class Note(Model):
+    author = ForeignKey(User, null=True, blank=False, on_delete=SET_NULL)
     title = CharField(max_length=255)
     text = TextField(blank=True)
     created = DateTimeField(auto_now_add=True)
