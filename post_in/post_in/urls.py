aad1ac123a4d73871daf48ca700d5e1fd07c5189
oleg <jf2@ua.fm>
2019-05-26 11:06:26 +0300

    Добивил авторизацию по api

--- post_in/post_in/urls.py
+++ post_in/post_in/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
      path('admin/', admin.site.urls),
      path('api/', include('api.urls')),
+     path('api-auth/', include(('rest_framework.urls', 'rest_framework')))
 ]
