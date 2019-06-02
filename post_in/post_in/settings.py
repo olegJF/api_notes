aeb69fe4e926073d4cfe48bfb0d54272e13dc478
oleg <jf2@ua.fm>
2019-06-02 13:14:29 +0300

    Базовая авторизация для браузерной версии API

--- post_in/post_in/settings.py
+++ post_in/post_in/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        
+    )
+}
