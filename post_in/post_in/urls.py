ad01feafeb29bf5bdc49f095b4962550dce18e88
oleg <jf2@ua.fm>
2019-06-02 13:13:57 +0300

    Добавил авторизацию JWT

--- post_in/post_in/urls.py
+++ post_in/post_in/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-     path('admin/', admin.site.urls),
-     path('api/', include('api.urls')),
-     path('api-auth/', include(('rest_framework.urls', 'rest_framework')))
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api-auth/', include(('rest_framework.urls', 'rest_framework'))),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
