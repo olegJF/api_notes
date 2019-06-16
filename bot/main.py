5063fddfc2c3901aaed9c76f88e0a40e750d3d66
oleg <jf2@ua.fm>
2019-06-16 15:56:49 +0300

    Обработка входящих сообщений по запросу доступных городов или специальностей

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from flask import Flask
 from flask.views import MethodView
 from flask import request
@@ -8,7 +9,14 @@ load_dotenv()
 
 app = Flask(__name__)
 TOKEN = os.environ.get('TOKEN')
+API_URL = os.environ.get('API_URL')
 TELEGRAM_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+def get_data_from_api(command):
+    url = API_URL + command
+    session = requests.Session()
+    r = session.get(url).json()
+    return r
 
 def send_message(chat_id, msg):
     session = requests.Session()
@@ -20,15 +28,23 @@ def send_message(chat_id, msg):
 
 def parse_text(text_msg):
     '''/start /help, /city /sp, @kiyv @python'''
+    addresses = {'city': '/cities', 'sp': '/sp'}
+    command_p = r'/\w+'
+    message = 'Неверный запрос'
     if '/' in text_msg:
         if '/start' in text_msg or  '/help' in text_msg:
-            message = '''Для того, чтобы узнать, какие города доступны, отправьте в сообщении `/cities`. 
+            message = '''Для того, чтобы узнать, какие города доступны, отправьте в сообщении `/city`. 
             Чтобы узнать о доступных специальностях - отправьте `/sp` 
             Чтобы сделать запрос на сохраненные вакансии, отправьте в сообщении через пробел - @город @специальность. 
             Например так - `@kyiv @python`  '''
-        return message
+            return message
+        else:
+            command = re.search(command_p, text_msg).group().replace('/', '')
+            command = addresses.get(command, None)
+            return [command] if command else None
+        
     else:
-        return None
+        return message
 
 @app.route('/', methods=["POST", "GET"])
 def index():
@@ -48,10 +64,23 @@ class BotAPI(MethodView):
         text_msg = resp['message']['text']
         chat_id = resp['message']['chat']['id']
         tmp = parse_text(text_msg)
-        print(tmp)
+        # print(tmp)
         if tmp:
-            send_message(chat_id, tmp)
-        print(resp)
+            if len(tmp) > 10:
+                send_message(chat_id, tmp)
+            elif  len(tmp) == 1:
+                resp = get_data_from_api(tmp[0])
+                if resp:
+                    message = ''
+                    for d in resp:
+                        message += '#' + d['slug'] + '\n'
+                    if tmp[0] == '/sp':
+                        msg = "Доступные специальности: \n"
+                    else:
+                        msg = "Доступные города: \n"
+                send_message(chat_id, msg+message)
+
+        # print(resp)
         return '<h1> Hi Telegram_Class!!! </h1>'
 
 app.add_url_rule('/TOKEN/', view_func=BotAPI.as_view('bot'))
