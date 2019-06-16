1933a0069698f2b9b8fdabf051ea174c419e932c
oleg <jf2@ua.fm>
2019-06-16 12:28:00 +0300

    вьюха на основе классов

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.views import MethodView
 from flask import request
 
 app = Flask(__name__)
@@ -10,6 +11,18 @@ def index():
         print(resp)
         return '<h1> Hi Telegram!!! </h1>'
     return '<h1> Hi BOT!!! </h1>'
+
+class BotAPI(MethodView):
+
+    def get(self):
+       return '<h1> Hi BOT_Class!!! </h1>'
+
+    def post(self):
+        resp = request.get_json()
+        print(resp)
+        return '<h1> Hi Telegram_Class!!! </h1>'
+
+app.add_url_rule('/TOKEN/', view_func=BotAPI.as_view('bot'))
 
 if __name__ == '__main__':
     app.run()
